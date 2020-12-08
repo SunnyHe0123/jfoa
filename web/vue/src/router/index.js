35493572790f7d3ad25335796f3d41548a6ffceb
sunnyhe0123 <sunnyhe0123@outlook.com>
2020-12-08 14:25:48 +0800

    add router

---
+++ web/vue/src/router/index.js
@@ -0,0 +1,14 @@
+import VueRouter from 'vue-router'
+import Vue from "vue"
+import HelloWorld from "../components/HelloWorld";
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    mode: "history",
+    routes: [{
+        path: "/",
+        component: HelloWorld
+    }]
+})
+
